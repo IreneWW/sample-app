4faa2d34424a18c42da0cd78af1f22a4665aabaa
IreneWW <irene301104@gmail.com>
2025-11-23 23:33:10 +0800

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -8,15 +8,17 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip3 install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+cat > tempdir/Dockerfile << 'EOF'
+FROM tiangolo/uwsgi-nginx-flask:python3.9
+COPY ./static /app/static/
+COPY ./templates /app/templates/
+COPY sample_app.py /app/main.py
+EXPOSE 5050
+ENV LISTEN_PORT 5050
+CMD ["python", "/app/main.py"]
+EOF
 
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
